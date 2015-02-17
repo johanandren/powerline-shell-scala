e83060234d8a47b68c24e0c3b2ae2e68a5a95e94
Johan Andrén <johan.andren@mejsla.se>
2015-02-17 14:25:52 +0100

    Make sure cursor isn't moved for the color escapes in zsh

--- src/main/scala/powerline/shells/ZshPrompt.scala
+++ src/main/scala/powerline/shells/ZshPrompt.scala
@@ -4,13 +4,22 @@ import powerline._
 
 object ZshPrompt {
 
-  import BashPrompt._
+  // Bash prompt color escape string
+  val reset = "\u001b[0m"
+
+  def color(prefix: String, code: Int) = s"%{\u001b[$prefix;5;${code}m%}"
+
+  def fgcolor(code: Color) = color("38", code.n)
+
+  def bgcolor(code: Color) = color("48", code.n)
+
+  def styled(text: String, style: Style): String =
+    fgcolor(style.fg) + bgcolor(style.bg) + text
 
   def renderSection(section: Section) =
     section.segments.map(segment => styled(segment.text, segment.style))
 
   def render(prompt: Seq[Segment]): String =
     prompt.map(seg => styled(seg.text, seg.style)).mkString + reset
-
 
 }
