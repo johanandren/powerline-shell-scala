837253eb35df219d62981ef0846414a2688eb459
Johan Andrén <johan.andren@mejsla.se>
2015-09-16 14:33:53 +0200

    Support for detached HEAD

--- src/main/scala/powerline/vcs/GitRepo.scala
+++ src/main/scala/powerline/vcs/GitRepo.scala
@@ -7,7 +7,12 @@ import scala.sys.process._
 case class GitRepo(path: File) extends Repository {
 
   def status = {
-    val branchName = Process(Seq("git", "symbolic-ref", "--short", "HEAD"), Some(path)).!!.trim
+    val revParseOutput = Process(Seq("git", "rev-parse", "--abbrev-ref", "HEAD"), Some(path)).!!.trim
+
+    val branchName =
+      if (revParseOutput != "HEAD") revParseOutput
+      // detached, use short checksum
+      else Process(Seq("git", "rev-parse", "--short", "HEAD")).!!.trim
 
     val dirty = Process(Seq("git", "status", "--porcelain"), Some(path)).lineStream.toSeq.nonEmpty
     val behind = Process(Seq("git", "rev-list", "HEAD..origin", "--count"), Some(path)).!!.trim.toInt
