3b2c3912822bc19014f21736a05e9d8aca640d1a
Johan Andrén <johan.andren@mejsla.se>
2015-09-16 14:55:20 +0200

    Correct parsing of the current branch ahead and behind remote

--- src/main/scala/powerline/vcs/GitRepo.scala
+++ src/main/scala/powerline/vcs/GitRepo.scala
@@ -9,16 +9,21 @@ case class GitRepo(path: File) extends Repository {
   def status = {
     val revParseOutput = Process(Seq("git", "rev-parse", "--abbrev-ref", "HEAD"), Some(path)).!!.trim
 
-    val branchName =
-      if (revParseOutput != "HEAD") revParseOutput
+    if (revParseOutput != "HEAD") {
+      val label = revParseOutput
+      val remote = s"origin/$label"
+      val dirty = Process(Seq("git", "status", "--porcelain"), Some(path)).lineStream.toSeq.nonEmpty
+      val behind = Process(Seq("git", "rev-list", s"HEAD..$remote", "--count"), Some(path)).!!.trim.toInt
+      val ahead = Process(Seq("git", "rev-list", s"$remote..HEAD", "--count"), Some(path)).!!.trim.toInt
+
+      RepoStatus(dirty, behind, ahead, label)
+    } else {
+
       // detached, use short checksum
-      else Process(Seq("git", "rev-parse", "--short", "HEAD")).!!.trim
+      val label = Process(Seq("git", "rev-parse", "--short", "HEAD")).!!.trim
+      RepoStatus(false, 0, 0, label)
+    }
 
-    val dirty = Process(Seq("git", "status", "--porcelain"), Some(path)).lineStream.toSeq.nonEmpty
-    val behind = Process(Seq("git", "rev-list", "HEAD..origin", "--count"), Some(path)).!!.trim.toInt
-    val ahead = Process(Seq("git", "rev-list", "origin..HEAD", "--count"), Some(path)).!!.trim.toInt
-
-    RepoStatus(dirty, behind, ahead, branchName)
   }
 
   def close(): Unit = {
