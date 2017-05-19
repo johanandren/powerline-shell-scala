acb1ba045e833ebe7d03f4e1c2ce6c06936813cc
Johan Andrén <johan@markatta.com>
2017-05-19 10:16:35 +0200

    Some random changes

---
+++ src/test/scala/powerline/vcs/GitRepoSpec.scala
@@ -0,0 +1,14 @@
+package powerline.vcs
+
+import powerline.Specification
+
+class GitRepoSpec extends Specification {
+/*
+  "the git repo status parser" should "parse a status line" in {
+    val line = "## master...origin/master [ahead 8]"
+
+    val status = GitRepo.parseStatus(line)
+
+  }
+*/
+}
